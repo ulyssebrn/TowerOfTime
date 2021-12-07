c1fafa4907be454941fb4034e7963fa7c8c0d1e6
ulyssebrn <93604518+ulyssebrn@users.noreply.github.com>
2021-12-07 20:29:31 +0100

    Divers

--- Tower of Time Project/Assets/Scripts/Mover.cs
+++ Tower of Time Project/Assets/Scripts/Mover.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,9 +7,16 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
+    Ray lastRay;
+
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
