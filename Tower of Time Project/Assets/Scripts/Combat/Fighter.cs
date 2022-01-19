460f2861ad1ece305e270743240268e854c9ae71
Elie Diar <bensko@MacBook-Air-de.local>
2022-01-19 20:27:42 +0100

    AttackOnEnemy

--- Tower of Time Project/Assets/Scripts/Combat/Fighter.cs
+++ Tower of Time Project/Assets/Scripts/Combat/Fighter.cs
@@ -23,7 +23,13 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private static void AttackBehaviour()
+        {
+            GetComponent<Animator>().SetTrigger("attack");
         }
 
         private bool GetIsInRange()
@@ -40,6 +46,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        // Animation Event
+        void Hit()
+        {
+
         }
     }   
 }
